04f5314a7f926d4c5abe48144d97928f4f94406f
Aman Kumar <amanv228@gmail.com>
2021-07-18 11:20:51 +0530

    final2

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
                 backSpeed={60}
                 loop
                 />
-                <a href="/#" className="btn-main-offer">Contact Me</a>
+                <a href="../AK_resume.pdf" download="resume.pdf"  className="btn-main-offer" >Download Resume</a>
             </div>
             
         </div>
